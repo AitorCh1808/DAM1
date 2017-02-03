1c0b43f17282fb269e87b098c96727b3d9689f1d
dam1 <dam1@1DAM-12.IES.QUEVEDO-AD>
2017-02-03 12:30:05 +0100

    primer cambio en local

---
+++ JavaApplication17/src/javaapplication17/JavaApplication17.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication17;
+
+/**
+ *
+ * @author dam1
+ */
+public class JavaApplication17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println(" Es mi primer ejermplo de GitHub");
+        
+        
+        
+        
+        
+        
+    }
+    
+}
